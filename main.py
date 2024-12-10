2aecfc4e7afa4ea8f9135a28caab3dc9636545ac
mateuszuser <ciomcia.mateusz@gmail.com>
2024-12-10 09:41:52 +0000

    pushing a build trigger

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/bob")
+def bob():
+    return "bob"
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
